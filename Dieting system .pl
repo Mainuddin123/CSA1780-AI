90657accff143b52cdd1c712b44d85ff294d16fd
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2023-09-01 14:28:17 +0530

    Create Dieting system .pl

---
+++ Dieting system .pl
@@ -0,0 +1,9 @@
+% Define facts about diseases and recommended diets.
+diet(heart_disease, 'Low-sodium diet').
+diet(diabetes, 'Low-carb diet').
+diet(obesity, 'Low-calorie diet').
+
+% Query recommended diet based on disease.
+% Example queries:
+% ?- diet(heart_disease, Diet).
+% ?- diet(diabetes, Diet).
