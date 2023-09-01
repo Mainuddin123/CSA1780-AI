f7e82d3b3279bf5e52d70f161e04609fb1ab697a
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2023-09-01 14:29:22 +0530

    Create Fruit and color backtracking.pl

---
+++ Fruit and color backtracking.pl
@@ -0,0 +1,10 @@
+% Define facts about fruits and their colors.
+fruit_color(apple, red).
+fruit_color(banana, yellow).
+fruit_color(grape, purple).
+fruit_color(orange, orange).
+
+% Query for the color of a specific fruit.
+% Example queries:
+% ?- fruit_color(apple, Color).
+% ?- fruit_color(banana, Color).
