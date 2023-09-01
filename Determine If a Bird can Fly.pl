3eac43c70b6dc7cb4b02d7bb72da29761b2eda92
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2023-09-01 14:27:12 +0530

    Create Determine If a Bird can Fly.pl

---
+++ Determine If a Bird can Fly.pl
@@ -0,0 +1,9 @@
+% Define facts about birds and whether they can fly.
+can_fly(sparrow).
+can_fly(eagle).
+cannot_fly(penguin).
+
+% Query whether a specific bird can fly.
+% Example queries:
+% ?- can_fly(sparrow).
+% ?- cannot_fly(penguin).
