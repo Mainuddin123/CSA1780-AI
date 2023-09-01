fbbc0f7dba3611f9ac92c055875f3a9a412f1f72
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2023-09-01 14:23:55 +0530

    Create Student-Teacher.pl

---
+++ Student-Teacher.pl
@@ -0,0 +1,7 @@
+% Database of students, teachers, and their subject codes.
+student(john, math).
+student(mary, history).
+student(alex, physics).
+teacher(prof_smith, math).
+teacher(prof_doe, history).
+teacher(prof_jones, physics).
